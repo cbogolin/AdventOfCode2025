e9e147d103792d6e6ff2c5da4ee6bea49e8f60d4
cbogolin <chandlerbogolin@gmail.com>
2025-12-13 00:27:08 -0500

    im stuck, calling it a night for now

--- AdventOfCode2025/Day1/Day1.cs
+++ AdventOfCode2025/Day1/Day1.cs
@@ -1,4 +1,6 @@
-﻿namespace AdventOfCode2025.Day1;
+﻿using System.Diagnostics;
+
+namespace AdventOfCode2025.Day1;
 
 public class Day1 : DayBase
 {
@@ -10,24 +12,48 @@ public class Day1 : DayBase
         int counter = 0;
         foreach (var instruction in instructions)
         {
-            Console.WriteLine($"starting iteration...");
             var direction = instruction[0];
-            Console.WriteLine($"Direction is: {direction}");
-            
             var distance = int.Parse(instruction[1..]);
-            Console.WriteLine($"distance is: {distance}");
+            var timesPassedZero = 0;
+            var startingNumber = currentNumber;
+            var startingNumberZero = startingNumber == 0 ? 1 : 0;
             
-            currentNumber += direction switch { 'R' => distance, 'L' => -distance };
-            Console.WriteLine($"currentNumber is: {currentNumber}");
+            Console.WriteLine($"\nstarting number: {startingNumber}");
+            Console.WriteLine($"instruction: {instruction}");
+            
+            currentNumber += 
+                direction switch
+                {
+                    'R' => distance,
+                    'L' => -distance,
+                    _ => throw new ArgumentOutOfRangeException(nameof(direction))
+                };
+            
+            Console.WriteLine($"number after turning: {currentNumber}");
+            var numberAfterTurning = currentNumber;
+            var landsOnZero = currentNumber == 0 ? 1 : 0;
+            //timesPassedZero += Math.Abs(currentNumber / 100);
+            //currentNumber %= 100;
+            while (currentNumber < 0)
+            {
+                currentNumber += 100;
+                timesPassedZero ++;
+            }
 
-            currentNumber %= 100 ;
-            currentNumber = currentNumber < 0 ? currentNumber + 100 : currentNumber;
-            Console.WriteLine($"currentNumber is: {currentNumber}");
+            while (currentNumber > 99)
+            {
+                currentNumber -= 100;
+                timesPassedZero ++;
+            }
+            var thisRoundCounts = timesPassedZero + landsOnZero - startingNumberZero;
             
-            counter = currentNumber == 0 ? counter + 1 : counter;
-            Console.WriteLine($"counter is: {counter}");
-            
-            Console.WriteLine($"Ending iteration...");
+            Console.WriteLine($"timesPassedZero: {timesPassedZero}");
+            Console.WriteLine($"landsOnZero: {landsOnZero}");
+            Console.WriteLine($"startingNumberZero: {startingNumberZero}");
+            Console.WriteLine($"new starting Number after corrections: {currentNumber}");
+
+            Console.WriteLine($"total counts for this round: {thisRoundCounts}\n");
+            counter += thisRoundCounts;
         }
         
         Console.WriteLine($"Answer is: {counter}");
